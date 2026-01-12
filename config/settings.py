abb5d148ce3a6d89c6d145d2a66ad1e352534fb2
Nabiilah Putri Safa <putri@Nabiilahs-MacBook-Pro.local>
2026-01-12 22:54:58 +0700

    fix path & add cors

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", 
+]
+
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
