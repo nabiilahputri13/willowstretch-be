ff0e104bc4396279dbf335cea2eb05ac42c31359
Nabiilah Putri Safa <putri@Nabiilahs-MacBook-Pro.local>
2026-01-17 22:30:01 +0700

    Add isAdmin permission

---
+++ users/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+from .models import User
+
+class IsAdmin(BasePermission):
+
+    def has_permission(self, request, view):
+        # Izin ini strict: Cuma boleh kalau Login DAN Role-nya Admin
+        return request.user.is_authenticated and request.user.role == User.Role.ADMIN
