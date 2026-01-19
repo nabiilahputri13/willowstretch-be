91c8ef5553ca6b61ba6627429526863f44c44290
Nabiilah Putri Safa <putri@Nabiilahs-MacBook-Pro.local>
2026-01-19 23:12:35 +0700

    Fix lint

--- pricing/admin.py
+++ pricing/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+
 from .models import Package, UserSubscription
+
 
 @admin.register(Package)
 class PackageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'credits', 'duration_days')
+    list_display = ("name", "price", "credits", "duration_days")
+
 
 @admin.register(UserSubscription)
 class UserSubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'package', 'remaining_credits', 'expired_at', 'is_active')
-    list_filter = ('package',)
+    list_display = ("user", "package", "remaining_credits", "expired_at", "is_active")
+    list_filter = ("package",)
